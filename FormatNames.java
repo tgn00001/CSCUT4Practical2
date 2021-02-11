6be22e92eb2691e1f2b0192dc8143e82ef75a535
tgn000012 <77923246+tgn00001@users.noreply.github.com>
2021-02-11 12:50:00 +0000

    Task 1 done! accepts -u flag in any position, I/O files must be in that order

--- FormatNames.java
+++ FormatNames.java
@@ -11,11 +11,29 @@ import java.util.*;
 public class FormatNames {
 
     public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
-        // Initially it will be easier to hardcode suitable file names.
-        // Finally, add code to read the filenames as arguments from the command line.
-        File input = new File("input.txt");
-        File output = new File("output.txt");
+        if (args.length < 2) {
+            System.out.println("Error: too few arguments. Both an input file and an output file must be specified.");
+            return;
+        }
+        else if (args.length > 3) {
+            System.out.println("Error: too many arguments.");
+        }
+
+        boolean upperCase = false;
+        File input = null;
+        File output = null;
+
+        for (String arg : args) {
+            if (arg.equals("-u")) {
+                upperCase = true;
+            }
+            else if (input == null) {
+                input = new File(arg);
+            }
+            else if (output == null){
+                output = new File(arg);
+            }
+        }
 
         // try with file resources
         try (Scanner in = new Scanner(input); PrintWriter out = new PrintWriter(output)) {
@@ -33,7 +51,12 @@ public class FormatNames {
 
                 // format parts of the name and add it to the StringBuilder
                 for (int i = 0; i < tokens.size() - 1; i++) {
-                    name.append(tokens.get(i).substring(0, 1).toUpperCase()).append(tokens.get(i).substring(1)).append(" ");
+                    if (upperCase) {
+                        name.append(tokens.get(i).toUpperCase()).append(" ");
+                    }
+                    else {
+                        name.append(tokens.get(i).substring(0, 1).toUpperCase()).append(tokens.get(i).substring(1)).append(" ");
+                    }
                 }
 
                 // convert the final String to a date class
@@ -43,10 +66,9 @@ public class FormatNames {
                 // write formatted output to the file
                 out.printf("%1$-20s\t%2$td/%2$tm/%2$tY\n", name, date);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: File not found.");
+        } catch (FileNotFoundException | NullPointerException e) {
+            System.out.println("Error: Input file not found or output file not accessible.");
         }
-
     } // main
 
 } // FilesInOut
